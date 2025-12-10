822aed8d71c53c01b7ca130b8cffcc8c87fd9a8f
FrancescoCauchi <francesco.cauchi.j95523@mcast.edu.mt>
2025-12-10 21:08:05 +0100

    Add project files.

---
+++ Models/ItemValidating.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkImportRestaurants.Models
+{
+    public interface IItemValidating
+    {
+        List<string> GetValidators();
+        string GetCardPartial();
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkImportRestaurants.Models
+{
+    public class Restaurant : IItemValidating
+    {
+        [Key]
+        public string Id { get; set; }  // Example: R-1001
+
+        public string Name { get; set; }
+        public string OwnerEmailAddress { get; set; }
+        public string Status { get; set; } = "pending";
+
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public List<string> GetValidators()
+        {
+            return new List<string>
+            {
+                OwnerEmailAddress,
+                "siteadmin@example.com"
+            };
+        }
+
+        public string GetCardPartial()
+        {
+            return "_RestaurantCard";
+        }
+    }
+}
